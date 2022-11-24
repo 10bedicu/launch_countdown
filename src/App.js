07cdf411c7faf7b588de3e48725631c0e14d4e6f
Gigin George <mail.gigin@gmail.com>
2022-11-24 12:18:00 +0530

    Countdown V1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+const calculateCountdown = (targetDate) => {
+  const now = new Date();
+  const difference = targetDate - now;
+
+  if (difference > 0) {
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((difference / 1000 / 60) % 60);
+    const seconds = Math.floor((difference / 1000) % 60);
+    return {
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  } else {
+    return {};
+  }
+};
+
+const useCountdown = (targetDate) => {
+  const [countdown, setCountdown] = useState(calculateCountdown(targetDate));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountdown(calculateCountdown(targetDate));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return { countdown };
+};
 
 function App() {
+  // Countdown to 28th November 2022 at 16:00:00
+  const targetDate = new Date("2022-11-28T16:00:00");
+  const { countdown } = useCountdown(targetDate);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Card with Title "Check back for the 10BedICU Karnataka launch in:"  */}
+      <div className="flex items-center justify-center h-screen">
+        <div className="max-w-4xl text-center items-center justify-center">
+          <h5 className="card-title">
+            Check back for the 10BedICU Karnataka launch in:
+          </h5>
+
+          <div className="grid grid-flow-col gap-5 text-center auto-cols-max justify-center pt-12">
+            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+              <span className="countdown font-mono text-5xl">
+                <span style={{ "--value": countdown.days || 0 }}></span>
+              </span>
+              days
+            </div>
+            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+              <span className="countdown font-mono text-5xl">
+                <span style={{ "--value": countdown.hours || 0 }}></span>
+              </span>
+              hours
+            </div>
+            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+              <span className="countdown font-mono text-5xl">
+                <span style={{ "--value": countdown.minutes || 0 }}></span>
+              </span>
+              min
+            </div>
+            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+              <span className="countdown font-mono text-5xl">
+                <span style={{ "--value": countdown.seconds || 0 }}></span>
+              </span>
+              sec
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
