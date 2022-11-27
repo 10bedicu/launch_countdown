ad8d93dff3b862db0f9a3cf3dae5d36df28de5af
Gigin George <mail.gigin@gmail.com>
2022-11-27 19:07:31 +0530

    Add Launch Youtube

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import logo from "./10bedicu.webp";
+import logo from "./assets/10bedicu.webp";
+import config from "./assets/config.json";
 
 const calculateCountdown = (targetDate) => {
   const now = new Date();
@@ -23,59 +24,100 @@ const calculateCountdown = (targetDate) => {
 
 const useCountdown = (targetDate) => {
   const [countdown, setCountdown] = useState(calculateCountdown(targetDate));
+  const [isExpired, setIsExpired] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCountdown(calculateCountdown(targetDate));
+      const newCountdown = calculateCountdown(targetDate);
+      setCountdown(newCountdown);
+      setIsExpired(Object.keys(newCountdown).length === 0);
     }, 1000);
     return () => clearInterval(interval);
   }, [targetDate]);
 
-  return { countdown };
+  return { countdown, isExpired };
 };
 
-const targetDate = new Date("2022-11-28T16:00:00");
 function App() {
   // Countdown to 28th November 2022 at 16:00:00
-  const { countdown } = useCountdown(targetDate);
+  const [targetDate, setTargetDate] = useState(new Date(config.targetDate));
+  const { countdown, isExpired } = useCountdown(targetDate);
+
+  useEffect(() => {
+    fetch("config.json").then((response) => {
+      response.json().then((data) => {
+        setTargetDate(new Date(data.targetDate));
+      });
+    });
+  }, []);
 
   return (
     <div>
       {/* Card with Title "Check back for the 10BedICU Karnataka launch in:"  */}
       <div className="flex items-center justify-center h-screen">
         <div
-          className="max-w-md md:max-w-4xl p-12 bg-white rounded-lg shadow-xl flex justify-center flex-col items-center"
+          className={
+            "p-12 bg-white rounded-lg shadow-xl flex justify-center flex-col items-center w-full max-w-md" +
+            (isExpired ? " md:max-w-6xl" : "  md:max-w-2xl")
+          }
           style={{ backgroundColor: "rgb(20, 113, 122)" }}
         >
-          <img src={logo} className="w-24" alt="logo" />
-          <h5 className="text-white text-xl mt-8 text-center">
-            Check back for the 10BedICU Karnataka launch in:
-          </h5>
+          <img src={logo} className="w-24 mb-8" alt="logo" />
+          {isExpired ? (
+            <h1 className="text-2xl text-white font-bold mb-2 text-center">
+              Join us as we launch 10BedICU at 41 Locations across Karnataka
+            </h1>
+          ) : (
+            <>
+              <h5 className="text-2xl text-white font-bold mb-2 text-center">
+                Check back for the 10BedICU Karnataka launch in
+              </h5>
 
-          <div className="grid grid-flow-col gap-5 text-center auto-cols-max justify-center pt-12">
-            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-              <span className="countdown font-mono text-5xl">
-                <span style={{ "--value": countdown.days || 0 }}></span>
-              </span>
-              days
-            </div>
-            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-              <span className="countdown font-mono text-5xl">
-                <span style={{ "--value": countdown.hours || 0 }}></span>
-              </span>
-              hours
-            </div>
-            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-              <span className="countdown font-mono text-5xl">
-                <span style={{ "--value": countdown.minutes || 0 }}></span>
-              </span>
-              min
-            </div>
-            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-              <span className="countdown font-mono text-5xl">
-                <span style={{ "--value": countdown.seconds || 0 }}></span>
-              </span>
-              sec
+              <div className="grid grid-flow-col gap-5 text-center auto-cols-max justify-center pt-8">
+                <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                  <span className="countdown font-mono text-5xl">
+                    <span style={{ "--value": countdown.days || 0 }}></span>
+                  </span>
+                  days
+                </div>
+                <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                  <span className="countdown font-mono text-5xl">
+                    <span style={{ "--value": countdown.hours || 0 }}></span>
+                  </span>
+                  hours
+                </div>
+                <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                  <span className="countdown font-mono text-5xl">
+                    <span style={{ "--value": countdown.minutes || 0 }}></span>
+                  </span>
+                  min
+                </div>
+                <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                  <span className="countdown font-mono text-5xl">
+                    <span style={{ "--value": countdown.seconds || 0 }}></span>
+                  </span>
+                  sec
+                </div>
+              </div>
+            </>
+          )}
+
+          <div className="w-full mt-8">
+            <div style={{ position: "relative", paddingTop: "56.25%" }}>
+              <iframe
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  width: "100%",
+                  height: "100%",
+                }}
+                src="https://www.youtube.com/embed/f_YUGbw_Bnk"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
             </div>
           </div>
         </div>
